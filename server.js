6353cfbd419f049902dd030997c9b7e971e38076
eero <rautsie@gmail.com>
2017-03-15 01:21:21 +0200

    Refactoring server for react-router.browserHistory

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 
+// serve static assets normally
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// When using react-router.browserHistory, handle every other route with index.html
+// which will contain a script tag to application's JavaScript
+app.get('*', function(req, res) {
+  //res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, function() {
