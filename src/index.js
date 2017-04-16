976131413dc2c26710f7538c21239396c73e62e0
Eero Rautsi <Eero@Eeros-Air.wlan>
2017-04-16 14:02:31 +0300

    if user has token, authenticate automatically

--- src/index.js
+++ src/index.js
@@ -14,11 +14,20 @@ import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import Frontpage from './components/frontpage';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+//IF token, consider the user to be signed in
+if (token) {
+  //update application state: put action into dispatch and its sent to all reducers.
+  store.dispatch({ type: AUTH_USER });
+
+}
 
 ReactDOM.render(
   //Provider communicates with "connected components".
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Frontpage} />
